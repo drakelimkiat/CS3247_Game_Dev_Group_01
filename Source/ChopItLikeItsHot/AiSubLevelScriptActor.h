ea2345c6f9f185001cc061839f6ed7f1475ab9d9
Seunghwan Choi <dadadak4@gmail.com>
2018-02-28 07:37:04 +0800

    Add SpawnEnemyAverageJoe() in AAiSubLevelScriptActor

--- Source/ChopItLikeItsHot/AiSubLevelScriptActor.h
+++ Source/ChopItLikeItsHot/AiSubLevelScriptActor.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Engine/LevelScriptActor.h"
+#include "EnemyAverageJoe.h"
 #include "AiSubLevelScriptActor.generated.h"
 
 /**
@@ -13,8 +14,30 @@ UCLASS()
 class CHOPITLIKEITSHOT_API AAiSubLevelScriptActor : public ALevelScriptActor
 {
 	GENERATED_BODY()
+
+public:
+	// Sets default values for this level-script-actor's properties
+	AAiSubLevelScriptActor();
+
+	// Called to spawn an instance of the EnemyAverageJoe class
+	UFUNCTION(BlueprintCallable, Category = Custom)
+	void SpawnEnemyAverageJoe();
 	
-	
-	
-	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = AiSubLevel)
+	float SpawnEnemyRadius;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = AiSubLevel)
+	TSubclassOf<AEnemyAverageJoe> BpEnemyAverageJoe;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	FTimerHandle MemberTimerHandle;
+
+	float EnemyAverageJoeHalfHeight;
+
+
+
 };
