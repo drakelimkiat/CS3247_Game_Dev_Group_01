2813f75db9cc7e7f615ce9c7abf492a63e301875
Seunghwan Choi <dadadak4@gmail.com>
2018-02-27 22:43:04 +0800

    Add Sublevel_AI

---
+++ Source/ChopItLikeItsHot/AiSubLevelScriptActor.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/LevelScriptActor.h"
+#include "AiSubLevelScriptActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHOPITLIKEITSHOT_API AAiSubLevelScriptActor : public ALevelScriptActor
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
